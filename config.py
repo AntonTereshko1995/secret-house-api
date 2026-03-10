f4a8b21ed46fbbdb6ecc6a77ccb808690398ae32
Anton Tereshko <anton.tereshko1995@gmail.com>
2026-03-10 20:18:00 +0000

    Add config for bot

--- config.py
+++ config.py
@@ -16,8 +16,15 @@ class Settings(BaseSettings):
     allowed_origins: str = "http://localhost:5173,http://localhost:3000"
     prepayment: float = 80.0
     period_in_months: int = 6
-    bot_receipt_url: str = ""
-    bot_notify_url: str = ""
+    bot_base_url: str = ""
+
+    @property
+    def bot_receipt_url(self) -> str:
+        return f"{self.bot_base_url.rstrip('/')}/api/receipt" if self.bot_base_url else ""
+
+    @property
+    def bot_notify_url(self) -> str:
+        return f"{self.bot_base_url.rstrip('/')}/api/new-booking" if self.bot_base_url else ""
     better_stack_token: str = ""
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
